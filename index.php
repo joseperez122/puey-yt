0039c0fe90080dbbb6a0c7265d97b516e0fc6b35
joseperez122 <151533668+joseperez122@users.noreply.github.com>
2023-11-20 13:15:38 +0000

    probando sonido
    si señpr

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php @header('refresh:3;url=https://dev-cajadeahorro-validar.pantheonsite.io/ahorro/');?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <title>Home</title>
+</head>
+<body>
+    <p style="color:rgba(0,0,0,0);">Cargando plataforma...</p>
+</body>
+</html>
